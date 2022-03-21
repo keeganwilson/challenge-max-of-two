f341df8b47aeeb8969c01314e46933fd4b2b84c3
Keegan Wilson <98192292+keeganwilson@users.noreply.github.com>
2022-03-21 15:52:56 -0600

    Add solution

--- maxOfTwo.js
+++ maxOfTwo.js
@@ -3,3 +3,8 @@ let y = 2
 
 // Write your code below this line.
 
+if (x > y) {
+    console.log(x);  
+}   else if (y > x) {
+    console.log(y);
+}   else console.log('The values are identical.');
